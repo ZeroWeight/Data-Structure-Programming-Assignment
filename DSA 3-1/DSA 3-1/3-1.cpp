80895582c224391f8f4dec4cb72b79532f12c426
ZeroWeight <zwt15@mails.tsinghua.edu.cn>
2016-05-05 22:44:28 +0800

    sort finish

--- DSA 3-1/DSA 3-1/3-1.cpp
+++ DSA 3-1/DSA 3-1/3-1.cpp
@@ -1,6 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#include<algorithm>
+//#include<algorithm>
 int value[100001];//from 1~n cities
 typedef int E[3];//[0] start [1] end [2] price
 E edge[100000];
@@ -15,16 +16,16 @@ int main() {
 	for (--m;~m;--m) {
 		scanf("%d %d %d", &edge[m][0], &edge[m][1], &edge[m][2]);
 	}
-	std::sort(edge, edge + M, cmp);
+	qsort(edge, M,sizeof(E), cmp);
 	for (m = M - 1;~m;m--) {
-		printf("%d %d %d", edge[m][0], edge[m][1], edge[m][2]);
+		printf("%d %d %d\n", edge[m][0], edge[m][1], edge[m][2]);
 	}
 	return 0;
 }
 int cmp(const void * p, const void * q) {
-	if (((E*)p)[0] < ((E*)q)[0]) return -1;
-	else if(((E*)p)[0] > ((E*)q)[0]) return 1;
-	else if (((E*)p)[1] < ((E*)q)[1]) return -1;
-	else if (((E*)p)[1] > ((E*)q)[1]) return 1;
+	if (((*(E*)p))[0] < ((*(E*)q))[0]) return -1;
+	else if (((*(E*)p))[0] > ((*(E*)q))[0]) return 1;
+	else if (((*(E*)p))[1] < ((*(E*)q))[1]) return -1;
+	else if (((*(E*)p))[1] > ((*(E*)q))[1]) return 1;
 	else return 0;
 }
