3da1f77b65f692a272bed1becc415658771f28d8
ZeroWeight <zwt15@mails.tsinghua.edu.cn>
2016-05-07 09:46:22 +0800

    bitmap finish

--- DSA3-2/DSA3-2/3-2.cpp
+++ DSA3-2/DSA3-2/3-2.cpp
@@ -1,13 +1,14 @@
 #include<stdio.h>
-// 1-> 1<<20
-//2 3->1<<19
-//4 5 6 7->1<<18   
-//..
-//from 1<<20 n>>= 20-- until n==1
-//father   n>>1
-//son n<<1,n<<1+1
-//total:1<<21 2097152
-//in char: 1<<18 262144
-//(bitmap[next >> 3] & (0x80 >> (next & 0x07))) check next
-//bitmap[i >> 3] |= (0x80 >> (i & 0x07)) write next to 1
-//bitmap[i>>3]&=~(0x80 >> (i & 0x07)) write to 0
+#include<iostream>
+#define check(n) (bitmap[n >> 3] & (0x80 >> (n & 0x07)))
+#define reverse(n) (bitmap[n >> 3] ^= (0x80 >> (n & 7)))
+char bitmap[1 << 18];
+int main() {
+	reverse(100);
+	reverse(101);
+	std::cout << check(100) << std::endl;
+	std::cout << check(101) << std::endl;
+	std::cout << check(102) << std::endl;
+	reverse(100);
+	std::cout << check(100) << std::endl;
+}
