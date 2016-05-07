b8d00bae98c48559be04604e152bc2ef7675e356
ZeroWeight <zwt15@mails.tsinghua.edu.cn>
2016-05-07 13:13:58 +0800

    final debug

--- DSA3-2/DSA3-2/3-2.cpp
+++ DSA3-2/DSA3-2/3-2.cpp
@@ -1,79 +1,65 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-//#include<iostream>
+#include <stdio.h>
 #include<stdlib.h>
 #define Check(n) (bitmap[n >> 3] & (0x80 >> (n & 0x07)))// check the bit
 #define Reverse(n) (bitmap[n >> 3] ^= (0x80 >> (n & 7)))//reverse the bit
-#define Set(n) (ever_reach[n >> 3] |= (0x80 >> (n & 0x07)))// set the bit to 1
-#define Ever(n) (ever_reach[n >> 3] & (0x80 >> (n & 0x07)))
-#define Father(n) (n>>1)
-#define Lchild(n) (n<<1)
 #define Rchild(n) ((n<<1)+1)
+#define Lchild(n) (n<<1)
 char bitmap[1 << 18];
-char ever_reach[1 << 18];
-int find_start[1 << 21];
-int find_end[1 << 21];
-void initial(int,int,int a=1);
-void reverse(int, int,int root=1);
-int query(int, int, int root = 1);
+int sum[1 << 21];
+void initial(const int, const int, const int);
+inline void pushdown(const int, const int, const int);
+void reverse(const int, const int, const int, const int, const int);
+int query(const int, const int, const int, const int, const int);
 int main() {
 	char* read_in = (char*)malloc(sizeof(char)*(1 << 20));
-	setvbuf(stdin, read_in, _IOFBF, 1 << 20);
-	int n, m;
-	int l, r;
 	register char op;
+	register int n, m, l, r;
+	setvbuf(stdin, read_in, _IOFBF, 1 << 20);
 	scanf("%d%d", &n, &m);
-	initial(1, n);
-	for (int i = 0; i < m; ++i) {
+	initial(1, n,1);
+	for (;m--;) {
 		scanf("\n%c%d%d", &op, &l, &r);
-		if (op == 'H') reverse(l, r);
-		else printf("%d\n", query(l, r));
+		if (op == 'H') reverse(l, r,1,n,1);
+		else printf("%d\n", query(l, r,1,n,1));
 	}
 	return 0;
 }
-void initial(int l, int r, int root) {
-	if (l == r) find_end[root] = find_start[root] = l;
-	else {
-		int mid = ((l + r) >> 1);
-		initial(l, mid, Lchild(root));
-		initial(mid + 1, r, Rchild(root));
-		find_start[root] = l;
-		find_end[root] = r;
+void initial(const int L, const int R , const int root) {
+	sum[root] = R - L + 1;
+	if (L == R) return;
+	int mid = (L + R) / 2;
+	initial(L, mid, root * 2);
+	initial(mid + 1, R, root * 2 + 1);
+}
+inline void pushdown(const int L, const int R, const int root) {
+	if (Check(root)) {
+		Reverse(Lchild(root));
+		Reverse(Rchild(root));
+		int mid = ((L + R) >> 1);
+		sum[Lchild(root)] = mid - L + 1 - sum[Lchild(root)];
+		sum[Rchild(root)] = R - mid - sum[Rchild(root)];
+		Reverse(root);
 	}
 }
-void reverse(int l, int r, int root) {
-	if (l > find_end[root] || r < find_start[root]) return;
-	if (l == find_start[root] && r == find_end[root]) {
+void reverse(const int l, const int r, const int L, const int R , const int root) {
+	if (l <= L && R <= r) {
 		Reverse(root);
+		sum[root] = R - L + 1 - sum[root];
 		return;
 	}
-	int temp = find_end[Lchild(root)];
-	if (Check(root)) Reverse(Rchild(root)), Reverse(Lchild(root)),Reverse(root);
-	if (temp >= r) reverse(l, r, Lchild(root));
-	else if (temp < l) reverse(l, r, Rchild(root));
-	else {
-		reverse(l, temp, Lchild(root));
-		reverse(temp + 1, r, Rchild(root));
-	}
+	pushdown(L, R, root);
+	int mid = ((L + R) >> 1);
+	if (l <= mid) reverse(l, r, L, mid, root * 2);
+	if (r > mid) reverse(l, r, mid + 1, R, root * 2 + 1);
+	sum[root] = sum[root * 2] + sum[root * 2 + 1];
 }
-int query(int l, int r, int root) {
-	if (l > find_end[root] || r < find_start[root]) return 0;//ill
-	if (find_end[root] == find_start[root]) return (Check(root)==0);//leaf
-	if (l == find_start[root] && r == find_end[root]) {
-		if (Check(root))
-		{
-			return 0;
-		}
-		else return find_end[root] - find_start[root] + 1;
-	}
-	if (Check(root)) Reverse(Rchild(root)), Reverse(Lchild(root)), Reverse(root);
-	int temp = find_end[Lchild(root)];
-	int tmp;
-	if (temp >= r) tmp = query(l, r, Lchild(root));
-	else if (temp < l) tmp = query(l, r, Rchild(root));
-	else {
-		tmp = query(l, temp, Lchild(root)) + query(temp + 1, r, Rchild(root));
-	}
-	if (Check(root)) tmp = r-l+1 - tmp;
-	return tmp;
+int query(const int l, const int r, const int L, const int R, const int root) {
+	if (l <= L && R <= r) return sum[root];
+	pushdown(L, R, root);
+	int res = 0, mid = (L + R) / 2;
+	if (l <= mid) res += query(l, r, L, mid, root * 2);
+	if (r > mid) res += query(l, r, mid + 1, R, root * 2 + 1);
+	return res;
 }
+
