5828e4ed66665c54fbc30171bfd0dbcf17583b8a
ZeroWeight <zwt15@mails.tsinghua.edu.cn>
2016-05-07 10:05:03 +0800

    bitmap tree finish

--- DSA3-2/DSA3-2/3-2.cpp
+++ DSA3-2/DSA3-2/3-2.cpp
@@ -2,6 +2,9 @@
 #include<iostream>
 #define check(n) (bitmap[n >> 3] & (0x80 >> (n & 0x07)))// check the bit
 #define reverse(n) (bitmap[n >> 3] ^= (0x80 >> (n & 7)))//reverse the bit
+#define father(n) (n>>1)
+#define Lchild(n) (n<<1)
+#define Rchild(n) ((n<<1)+1)
 char bitmap[1 << 18];
 inline int find_start(int n);//find the start of bit(include)
 inline int find_end(int n);//find the end of the bit(include)
