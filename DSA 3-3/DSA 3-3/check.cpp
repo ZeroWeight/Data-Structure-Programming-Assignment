3354125d22d864c0d3d2fd01d037ef3e855ec506
ZeroWeight <zwt15@mails.tsinghua.edu.cn>
2016-05-08 13:31:36 +0800

    3-3 add_ons

---
+++ DSA 3-3/DSA 3-3/check.cpp
@@ -0,0 +1,27 @@
+//#include<stdio.h>
+//#define TEST 10
+//#include<stdlib.h>
+//#include<time.h>
+//#include<iostream>
+//#include<algorithm>
+//int main() {  
+//	srand(time(0));
+//	int test = rand() % TEST;
+//	std::cout << TEST << '\t' << test << std::endl; 
+//	for (int i = 0;i < TEST;i++) {
+//		int a = rand() % TEST;
+//		int b = rand() % TEST;
+//		int t = rand() % TEST + 1;
+//		t *= 100;
+//		std::cout << a << '\t' << b << '\t' << t << std::endl;
+//	}
+//	for (int i = 0;i < test;i++) {
+//		int a1 = rand() % TEST;
+//		int a2 = rand() % TEST;
+//		int b1 = rand() % TEST;
+//		int b2 = rand() % TEST;
+//		std::cout << std::min(a1, a2) << '\t' << std::min(b1, b2) << '\t'
+//			<< std::max(a1, a2) << '\t' << std::max(b1, b2) << std::endl;
+//	}
+//	return 0;
+//}
