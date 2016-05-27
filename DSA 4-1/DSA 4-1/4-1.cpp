de072bb37c37100fea296c4b21094b3df64fb381
ZeroWeight <zwt15@mails.tsinghua.edu.cn>
2016-05-27 11:39:18 +0800

    law queue finish reaady to use B_BFS

---
+++ DSA 4-1/DSA 4-1/4-1.cpp
@@ -0,0 +1,114 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#define RESULT 001234567
+typedef struct _list{
+	int value;
+	struct _list* next;
+}list;
+class queue {
+private:
+	list* head;
+	list* end;
+	int num;
+public:
+	queue(int);
+	void push(int);
+	int pop();
+	int empty();
+};
+int solve(int);
+inline int change_1(int);
+inline int change_2(int);
+inline int change_3(int);
+int map[077777777];
+int main() {
+	int n;
+	int i;
+	int temp;
+	int code;
+	scanf("%d", &n);
+	for (;n--;) {
+		code = 0;
+		//read as 8-bit;
+		for (i = 8;i--;) {
+			scanf("%d", &temp);
+			code <<= 3;
+			code += (temp - 1);
+		}
+		printf("%o\n",code);
+	}
+	return 0;
+}
+int solve(int code) {
+	//find the code,if not
+	//A while loop
+	//BFS the result root tree a loop
+	//BFS the begin root tree a loop
+	//check whether they have a cir.
+	//if cannot goon return -1;
+	
+	return -1;
+}
+inline int change_1(int code) {
+	int temp = 0;
+	for (int i = 8;i--;) {
+		temp <<= 3;
+		temp += (code & 07);
+		code >>= 3;
+	}
+	return temp;
+}
+inline int change_2(int code) {
+	int lower = code & 07777;//the lower state;
+	int higher = code >> (3 * 4);
+	int lower_1 = (lower & 07000)>>(3*3);//the [8]
+	int lower_234 = lower - (lower_1<<(3*3));
+	lower = (lower_234 << 3) + lower_1;
+	int higher_1 = higher & 07;//the [8]
+	int higher_234 = higher >> 3;
+	higher = (higher_1 << (3 * 3)) + higher_234;
+	return ((higher << (3 * 4)) + lower);
+}
+inline int change_3(int code) {
+	int a[8];
+	for (int i = 8;i--;) {
+		a[i] = code & 07;
+		code >>= 3;
+	}
+	int temp = a[1];
+	a[1] = a[6];
+	a[6] = a[5];
+	a[5] = a[2];
+	a[2] = temp;
+	temp = 0;
+	for (int i = 0;i < 8;++i) {
+		temp <<= 3;
+		temp += a[i];
+	}
+	return temp;
+}
+int queue::empty() {
+	return num == 0;
+}
+queue::queue(int code) {
+	this->head = this->end = (list*)malloc(sizeof(list));
+	this->head->value = code;
+	this->head->next = NULL;
+	this->num = 1;
+}
+int queue::pop() {
+	list* temp = this->head;
+	this->head = this->head->next;
+	this->num--;
+	int tmp = temp->value;
+	free(temp);
+	return tmp;
+}
+void queue::push(int code) {
+	this->end->next = (list*)malloc(sizeof(list));
+	this->end = this->end->next;
+	this->end->value = code;
+	this->end->next = NULL;
+	this->num++;
+}
