3df758e55f883fef395b7b28f691c0d2ef9df388
ZeroWeight <zwt15@mails.tsinghua.edu.cn>
2016-05-27 10:38:28 +0800

    how to move  OK

--- DSA 3-4/DSA 3-4/3-4.cpp
+++ DSA 3-4/DSA 3-4/3-4.cpp
@@ -1,40 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
-#include <cstring>
-const int N = 10000 + 5;
-char hash[N][9];
-int tot, cnt[N], head[N], next[N];
-int calc_hash(char *s) {
-	int key = 0, len = strlen(s);
-	for (int i = 0; i < len; ++i) key += s[i] * (i + 1) % (N - 1) + 1;
-	return key;
-}
-int create_hash(char *s) {
-	int key = calc_hash(s);
-	memcpy(hash[++tot], s, sizeof s);
-	next[tot] = head[key];
-	head[key] = tot;
-	return tot;
-}
-int find_hash(char *s) {
-	int key = calc_hash(s);
-	for (int i = head[key]; i; i = next[i])
-		if (!strcmp(s, hash[i])) return i;
-	return 0;
-}
-int main() {
+#include <stdio.h>
+#include<string.h>
+const int N = 1 << 14;
+char name[N][9];
+char read_in[1 << 20];
+int tot, count[N], head[N], next[N];
+int zip(char *s);
+int create(char *s);
+int find(char *s);
+int main(){
+	setvbuf(stdin, read_in, _IOFBF, 1 << 20);
 	int n, max = 0;
 	scanf("%d", &n);
-	char s[9], name[9];
-	while (n--) {
+	register char s[9];
+	char* max_name;
+	for (;n--;) {
 		scanf("\n%s", s);
-		int hash_value = find_hash(s);
-		if (!hash_value) hash_value = create_hash(s);
-		if (++cnt[hash_value] > max) {
-			max = cnt[hash_value];
-			memcpy(name, s, sizeof s);
+		int hash_value = find(s);
+		if (!hash_value) hash_value = create(s);
+		if (++count[hash_value] > max){
+			max = count[hash_value];
+			max_name = name[hash_value];
 		}
 	}
-	printf("%s %d\n", name, max);
+	printf("%s %d\n", max_name, max);
 	return 0;
+}
+int zip(char *s) {
+	int key = 0;
+	for (int i = 0; s[i]; ++i) {
+		key += (s[i] - 'a' + 1) & 7;
+		key <<= 3;
+	}
+	key >>= 3;
+	key &= (0x3FFF);
+	return (int)key;
+}
+int create(char *s) {//if never found a hash code create it with a single list
+	int key = zip(s);
+	memcpy(name[++tot], s, sizeof s);
+	next[tot] = head[key];//add the following list
+	head[key] = tot;//create the node
+	return tot;
+}
+int find(char *s) {//check whether the hash code has a string
+	int key = zip(s);
+	for (int i = head[key]; i; i = next[i])
+		if (!strcmp(s, name[i])) return i;
+	return 0;//if fail return 0
 }
