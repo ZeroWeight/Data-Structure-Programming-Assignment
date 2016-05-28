484e2342bdd4317a169f5d7f42478da33ec4d82d
ZeroWeight <zwt15@mails.tsinghua.edu.cn>
2016-05-28 17:04:23 +0800

    why only one?!

--- DSA 4-3/DSA 4-3/4-3.cpp
+++ DSA 4-3/DSA 4-3/4-3.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _DEBUG_
 #include<stdio.h>
 #include<malloc.h>
 void build(int*, int);
@@ -5,21 +7,43 @@ void fix(int*, int, int);
 inline void swap(int*a, int*b) { int temp = *a;*a = *b;*b = temp; }
 int main() {
 	int n, k;
+#ifndef _DEBUG_
 	fread(&n, sizeof(int), 1, stdin);
+#endif
+#ifdef _DEBUG_
+	scanf("%d", &n);
+#endif
+#ifndef _DEBUG_
 	fread(&k, sizeof(int), 1, stdin);
+#endif
+#ifdef _DEBUG_
+	scanf("%d", &k);
+#endif
 	int *data = new int[k + 1];
+#ifndef _DEBUG_
 	fread(data + 1, sizeof(int), n, stdin);
+#endif
+#ifdef _DEBUG_
+	for (int i = 1;i < k + 1;i++) {
+		scanf("%d", data + i);
+	}
+#endif
 	build(data, k);
 	for (int i = n - k;i--;) {
+#ifndef _DEBUG_
 		fread(data, sizeof(int), 1, stdin);
+#endif
+#ifdef _DEBUG_
+		scanf("%d", data);
+#endif
 		if (data[0] < data[1]) {
 			data[1] = data[0];
 			fix(data, 1, k);
 		}
 	}
-	for (++k;k--;) {
+	for (;k--;) {
 		printf("%d\n", data[1]);
-		data[1] = data[k];
+		data[1] = data[k+1];
 		fix(data, 1, k);
 	}
 	return 0;
