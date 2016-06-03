e98edd7b8f2dfc98f7cc224e3354a58f71ae6a1c
ZeroWeight <zwt15@mails.tsinghua.edu.cn>
2016-06-03 15:40:06 +0800

    wrong and Archer joined

--- DSA 4-3/DSA 4-3/4-3.cpp
+++ DSA 4-3/DSA 4-3/4-3.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define InHeap(n,i) (((-1)<(i))&&((i)<(n))
+#define InHeap(n,i) (((-1)<(i))&&((i)<(n)))
 #define Parent(i) ((i-1)>>1)
 #define LastLinternal(n) Parent(n-1)
 #define LChild(i) (1+((i)<<1))
@@ -10,7 +10,7 @@
 #define RChildVaild(n,i) InHeap(n,RChild(i))
 #define Bigger(PQ,i,j) ((PQ[i]<PQ[j])?j:i)
 #define ProperParent(PQ,n,i) \
-		(RChildVaild(n,i)?Bigger(PQ,Bigger(i,LChild(i)),RChild(i)): \
+		(RChildVaild(n,i)?Bigger(PQ,Bigger(PQ,i,LChild(i)),RChild(i)): \
 		(LChildVaild(n,i)?Bigger(PQ,i,LChild(i)): i \
 		) \
 		)
@@ -21,8 +21,7 @@ class PQ_ComplHeap {
 private:
 	T* data;
 	Rank size;
-	Rank percolateDown(Rank, Rank);
-	Rank percolateUp(Rank);
+	inline Rank percolateDown(Rank, Rank);
 	void heapify(Rank);
 public:
 	PQ_ComplHeap(int);
@@ -30,18 +29,65 @@ public:
 	inline T const getMax();
 	T delmax();
 };
+int main() {
+	int n, k;
+	fread(&n, sizeof(int), 1, stdin);
+	fread(&k, sizeof(int), 1, stdin);
+	PQ_ComplHeap H(k);
+	for (int i = n - k;i--;) {
+		fread(&n, sizeof(int), 1, stdin);
+		if (n < H.getMax()) H.insert(n);
+	}
+	for (int i = k;i--;) {
+		printf("%d\n", H.delmax());
+	}
+}
 inline T const PQ_ComplHeap::getMax() {
 	return data[0];
 }
 void PQ_ComplHeap::insert(T e) {
 	data[0] = e;
-	percolateDown(0, size);
+	percolateDown(size, 0);
 }
-Rank PQ_ComplHeap::percolateUp(Rank i) {
-	while (ParentValid(i)) {
-		Rank j = Parent(i);
-		if (data[i] < data[j]) break;
-		swap(data[i], data[j]);i = j;
+T PQ_ComplHeap::delmax() {
+	T maxelem = data[0];
+	data[0] = data[--size];
+	percolateDown(size, 0);
+	return maxelem;
+}
+Rank PQ_ComplHeap::percolateDown(Rank n, Rank i) {
+	Rank j = i;
+	T max;
+	T temp = data[i];
+	if (RChildVaild(n, i)) {
+		if (data[j] < data[LChild(i)]) j = LChild(i);
+		if (data[j] < data[RChild(i)]) j = RChild(i);
 	}
+	else if (LChildVaild(n, i)) {
+		if (data[j] < data[LChild(i)]) j = LChild(i);
+	}
+	while (i != j) {
+		data[i] = data[j];
+		i = j;
+		if (RChildVaild(n, i)) {
+			max = (data[LChild(i)]>data[RChild(i)]) ? i : j;
+			if (temp < data[max]) j = max;
+		}
+		else if (LChildVaild(n, i)) {
+			if (temp < data[LChild(i)]) j = LChild(i);
+		}
+	}
+	data[j] = temp;
 	return i;
+}
+PQ_ComplHeap::PQ_ComplHeap(int k) {
+	data = (T*)malloc(sizeof(T)*k);
+	size = k;
+	fread(data, sizeof(T), k, stdin);
+	heapify(k);
+}
+void PQ_ComplHeap::heapify(Rank n) {
+	for (int i = LastLinternal(n);InHeap(n, i);--i) {
+		percolateDown(n, i);
+	}
 }
